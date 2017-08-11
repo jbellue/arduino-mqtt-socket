a406202d840269a20b54c40b6f8b54326e3a658a
jbellue <jbellue@gmail.com>
2017-08-11 20:14:06 +0100

    adding first go at webserver

--- arduino-mqtt-socket.ino
+++ arduino-mqtt-socket.ino
@@ -2,12 +2,14 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
+#include <ESP8266WebServer.h>
 
 #include "wifi_credentials.h"
 /* contains:
   const char* wifi_ssid = "wifissid";
   const char* wifi_password = "wifipassword";
 */
+
 
 const char* mqtt_server = "192.168.1.24";
 const char* mqtt_topic = "led_1";
@@ -19,6 +21,8 @@ bool relay_state = LOW;
 Bounce debouncer = Bounce();
 
 WiFiClient espClient;
+ESP8266WebServer server(80);
+
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
@@ -33,7 +37,57 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  server.on("/",       handle_root);
+  server.on("/submit", handle_submit);
+  server.begin();
   ArduinoOTA.begin();
+}
+
+void handle_root() {
+  send_page();
+}
+
+void send_page() {
+  char local_ip[16];
+  sprintf(local_ip, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3] );
+  String page = "<!DOCTYPE html><html><body><form action='http://";
+  page += local_ip;
+  page += "/submit' method='POST'>MQTT broker: <input type='text' name='broker' value='";
+  page += mqtt_server;
+  page += "'><br>MQTT topic: <input type='text' name='topic' value='";
+  page += mqtt_topic;
+  page += "'><br>Relay:<br><input type='radio' name='relay' value='on'";
+  if(relay_state == HIGH) {
+    page += " checked";
+  }
+  page += ">on<br><input type='radio' name='relay' value='off'";
+    if(relay_state == LOW) {
+    page += " checked";
+  }
+  page += ">off<br><br><input type='submit' value='Submit'></form></body></html>";
+  server.send(200, "text/html", page);
+}
+void handle_submit() {
+  if (server.args() > 0 ) {
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+      if (server.argName(i) == "broker") {
+        
+      } else if (server.argName(i) == "topic") {
+        
+      } else if (server.argName(i) == "relay") {
+        if (server.arg(i) == "on") {
+          relay_state = HIGH;
+          client.publish(mqtt_topic, "1");
+        }
+        else {
+          relay_state = LOW;
+          client.publish(mqtt_topic, "0");
+        }
+        digitalWrite(relay_pin, relay_state);
+      }
+    }
+  }
+  send_page();
 }
 
 void setup_wifi() {
@@ -56,6 +110,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -75,7 +130,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.println("relay_pin -> HIGH");
     relay_state = HIGH;
   }
-
 }
 
 void reconnect() {
@@ -98,12 +152,14 @@ void reconnect() {
     }
   }
 }
+
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
   ArduinoOTA.handle();
   client.loop();
+  server.handleClient();
 
   debouncer.update();
   if ( debouncer.fell()) {
